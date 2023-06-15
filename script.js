4acd2136ddb8005e292ca9bf520344ee87d9ffb0
Harsh <hgaur491@gmail.com>
2023-06-15 10:36:17 +0530

    navbar and hero section completeting

---
+++ script.js
@@ -0,0 +1,9 @@
+const $ = (e, i) =>i ? document.querySelectorAll(`${e}`) :document.querySelector(`${e}`);
+
+// navbar toggle
+$("#menuBtn").addEventListener("click",()=>{
+    $(".navlinks_outer").style.left="0rem";
+});
+$("#clossBtn i").addEventListener("click",()=>{
+    $(".navlinks_outer").style.left= "-10rem";
+});
